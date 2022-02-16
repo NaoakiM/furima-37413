666e4b46c8f146c02e6ce4581c8a42fc4ca142d9
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-02-16 21:34:57 +0900

    出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,20 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  # before_action :move_to_index, except: [:index]
   def index
     # @items = Item.all
+  end
+
+  def new
+    # @items = Item.all
+  end
+
+  def creat
+    # @items = Item.all
+    @furima = Furima.new(furima_params)
+    if @furima.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
