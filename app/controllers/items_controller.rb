c1bbf77553b04ce05a2740cb20109f706e71fe4f
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-02-23 17:45:11 +0900

    商品削除機能修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
     private
   def furima_params
     params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price, :image).merge(user_id: current_user.id)
