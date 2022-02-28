44ceecc12f8b570a1508f9802b7b51f072df9f1d
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-02-28 18:34:55 +0900

    レビュー修正4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :create]
-  before_action :set_item,only: [:index, :create, :show]
-  before_action :move_to_index, only: [:index, :show]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item,only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @purchase_shipping = PurchaseShipping.new
--- spec/factories/purchase_shippings.rb
+++ spec/factories/purchase_shippings.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
-  factory :purchase_shipping do
     postal_code_id { '123-4567' }
     item_prefecture_id { 1 }
     city { "札幌市" }
@@ -7,6 +6,4 @@ FactoryBot.define do
     building {}
     phone_number { "09000000000" }
     token {"tok_abcdefghijk00000000000000000"}
-    
-  end
 end
