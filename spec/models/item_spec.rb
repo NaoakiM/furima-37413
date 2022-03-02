74eb9f98bf5c96e7c16d5b958c1e3207242201c2
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-03-02 11:55:55 +0900

    追加実装　商品出品機能エラーを日本語に

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,79 +19,79 @@ RSpec.describe Item, type: :model do
     it '商品画像が空では出品出来ない' do
       @item.image = nil
       @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
+      expect(@item.errors.full_messages).to include("Imageを入力してください")
     end
 
     it 'userが紐付いていなければ登録できないこと' do
       @item.user = nil
       @item.valid?
-      expect(@item.errors.full_messages).to include("User must exist")
+      expect(@item.errors.full_messages).to include("Userを入力してください")
     end
 
     it '商品名が空だと保存できない' do
       @item.item_name = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item name can't be blank")
+      expect(@item.errors.full_messages).to include("Item nameを入力してください")
     end
 
     it '商品の説明が空だと保存できない' do
       @item.item_info = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item info can't be blank")
+      expect(@item.errors.full_messages).to include("Item infoを入力してください")
     end
 
     it '商品のカテゴリーの情報が空だと保存できない' do
       @item.item_category_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item category must be other than 1")
+      expect(@item.errors.full_messages).to include("Item categoryは1以外の値にしてください")
     end
 
     it '商品の状態の情報が空だと保存できない' do
       @item.item_sales_status_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item sales status must be other than 1")
+      expect(@item.errors.full_messages).to include("Item sales statusは1以外の値にしてください")
     end
 
     it '配送料の負担の情報が空だと保存できないこと' do
       @item.item_shipping_fee_status_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item shipping fee status must be other than 1")
+      expect(@item.errors.full_messages).to include("Item shipping fee statusは1以外の値にしてください")
     end
 
     it '発送元の地域の情報が空だと保存できないこと' do
       @item.item_prefecture_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item prefecture must be other than 1")
+      expect(@item.errors.full_messages).to include("Item prefectureは1以外の値にしてください")
     end
 
     it '発送までの日数の情報が空だと保存できないこと' do
       @item.item_scheduled_delivery_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item scheduled delivery must be other than 1")
+      expect(@item.errors.full_messages).to include("Item scheduled deliveryは1以外の値にしてください")
     end
 
     it 'priceが300円未満では保存できないこと' do
       @item.item_price = 299
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item price must be greater than or equal to 300")
+      expect(@item.errors.full_messages).to include("Item priceは300以上の値にしてください")
     end
 
     it 'priceが9,999,999円を超過すると保存できないこと' do
       @item.item_price = 10000000
       @item.valid?
-      expect(@item.errors.full_messages).to include('Item price must be less than or equal to 9999999')
+      expect(@item.errors.full_messages).to include('Item priceは9999999以下の値にしてください')
     end
 
     it 'priceが全角数字だと保存できないこと' do
       @item.item_price = '２０００'
       @item.valid?
-      expect(@item.errors.full_messages).to include('Item price is not a number')
+      expect(@item.errors.full_messages).to include('Item priceは数値で入力してください')
     end
 
     it 'priceが空だと保存できないこと' do
       @item.item_price = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item price is not a number")
+      expect(@item.errors.full_messages).to include("Item priceは数値で入力してください")
     end
   end
 end
