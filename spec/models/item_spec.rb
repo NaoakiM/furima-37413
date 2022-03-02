a344924737837b8dab5401b68580d19f35fe9d4e
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-03-02 14:27:27 +0900

    追加実装　商品出品機能エラーを日本語に2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,67 +31,67 @@ RSpec.describe Item, type: :model do
     it '商品名が空だと保存できない' do
       @item.item_name = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item nameを入力してください")
+      expect(@item.errors.full_messages).to include("商品名を入力してください")
     end
 
     it '商品の説明が空だと保存できない' do
       @item.item_info = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item infoを入力してください")
+      expect(@item.errors.full_messages).to include("商品の説明を入力してください")
     end
 
     it '商品のカテゴリーの情報が空だと保存できない' do
       @item.item_category_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item categoryは1以外の値にしてください")
+      expect(@item.errors.full_messages).to include("カテゴリーは1以外の値にしてください")
     end
 
     it '商品の状態の情報が空だと保存できない' do
       @item.item_sales_status_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item sales statusは1以外の値にしてください")
+      expect(@item.errors.full_messages).to include("商品の状態は1以外の値にしてください")
     end
 
     it '配送料の負担の情報が空だと保存できないこと' do
       @item.item_shipping_fee_status_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item shipping fee statusは1以外の値にしてください")
+      expect(@item.errors.full_messages).to include("配送料の負担は1以外の値にしてください")
     end
 
     it '発送元の地域の情報が空だと保存できないこと' do
       @item.item_prefecture_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item prefectureは1以外の値にしてください")
+      expect(@item.errors.full_messages).to include("発送元の地域は1以外の値にしてください")
     end
 
     it '発送までの日数の情報が空だと保存できないこと' do
       @item.item_scheduled_delivery_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item scheduled deliveryは1以外の値にしてください")
+      expect(@item.errors.full_messages).to include("発送までの日数は1以外の値にしてください")
     end
 
     it 'priceが300円未満では保存できないこと' do
       @item.item_price = 299
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item priceは300以上の値にしてください")
+      expect(@item.errors.full_messages).to include("価格は300以上の値にしてください")
     end
 
     it 'priceが9,999,999円を超過すると保存できないこと' do
       @item.item_price = 10000000
       @item.valid?
-      expect(@item.errors.full_messages).to include('Item priceは9999999以下の値にしてください')
+      expect(@item.errors.full_messages).to include('価格は9999999以下の値にしてください')
     end
 
     it 'priceが全角数字だと保存できないこと' do
       @item.item_price = '２０００'
       @item.valid?
-      expect(@item.errors.full_messages).to include('Item priceは数値で入力してください')
+      expect(@item.errors.full_messages).to include('価格は数値で入力してください')
     end
 
     it 'priceが空だと保存できないこと' do
       @item.item_price = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Item priceは数値で入力してください")
+      expect(@item.errors.full_messages).to include("価格は数値で入力してください")
     end
   end
 end
