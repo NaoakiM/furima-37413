a945ab0abde22d17da413e69100d853e201dbbaa
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-03-02 12:58:37 +0900

    追加実装　ユーザー新規登録エラー文日本語修正

--- spec/models/purchase_shipping_spec.rb
+++ spec/models/purchase_shipping_spec.rb
@@ -25,13 +25,13 @@ RSpec.describe PurchaseShipping, type: :model do
     it 'postal_code_idが空だと保存できないこと' do
       @purchase_shipping.postal_code_id = ''
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Postal code can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Postal codeを入力してください")
     end
 
     it 'postal_code_idが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
       @purchase_shipping.postal_code_id = '1234567'
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        expect(@purchase_shipping.errors.full_messages).to include("Postal codeis invalid. Include hyphen(-)")
     end
 
     it 'item_prefecture_idを選択していないと保存できないこと' do
@@ -43,55 +43,55 @@ RSpec.describe PurchaseShipping, type: :model do
     it 'cityが空だと保存できないこと' do
       @purchase_shipping.city = ''
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("City can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Cityを入力してください")
     end
 
     it 'addressesが空だと保存できないこと' do
       @purchase_shipping.addresses = ''
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Addresses can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Item prefecturecan't be blank")
     end
 
     it 'phone_numberが空だと保存できないこと' do
       @purchase_shipping.phone_number = ''
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Phone number can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Item prefecturecan't be blank")
     end
 
     it 'phone_numberが半角数値以外が入力されている場合' do
       @purchase_shipping.phone_number = '１２３４５６７８９０'
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Phone number can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Phone numbercan't be blank")
     end
 
     it 'user_idが空では登録できないこと' do
       @purchase_shipping.user_id = ''
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("User can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Userを入力してください")
     end
 
     it 'item_idが空では登録できないこと' do
       @purchase_shipping.item_id = ''
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Item can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Itemを入力してください")
     end
-    
+
     it 'tokenが空では登録できない' do
       @purchase_shipping.token = ''
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Token can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Tokenを入力してください")
     end
 
     it '電話番号は9桁以下では登録できないこと' do
       @purchase_shipping.phone_number = '123456789'
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Phone number can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Phone numbercan't be blank")
     end
 
     it '電話番号は12桁以上では登録できないこと' do
       @purchase_shipping.phone_number = '123456789012'
         @purchase_shipping.valid?
-        expect(@purchase_shipping.errors.full_messages).to include("Phone number can't be blank")
+        expect(@purchase_shipping.errors.full_messages).to include("Phone numbercan't be blank")
     end
   end
 end
